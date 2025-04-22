06ccd9910562f67d12e0b7775a7f97b398fa381d
Hastingsamo.com <hezekiahhastings14@gmail.com>
2025-04-22 15:01:12 +0100

    hoom

--- src/Pagess/CockTailss.jsx
+++ src/Pagess/CockTailss.jsx
@@ -10,7 +10,7 @@ function CockTailss() {
       <h1 className="text-3xl font-bold mb-4">Cocktail</h1>
       <p className="text-gray-700 mb-4">This is the Food page.</p>
       <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-        Click Me
+        Click M
       </button>
     </div>
   </div>
