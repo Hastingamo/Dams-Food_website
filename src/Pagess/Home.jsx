a6e6ca48e211a7b5bffa49f1fec83463ea2730ad
Hastingsamo.com <hezekiahhastings14@gmail.com>
2025-04-22 14:54:37 +0100

    refactor: remove unused motion.div from Home component

--- src/Pagess/Home.jsx
+++ src/Pagess/Home.jsx
@@ -7,9 +7,6 @@ function Home() {
     <div className="flex flex-row items-center justify-center h-screen bg-gray-100">
       <Sidebars/>
       <h1 className="text-4xl font-bold mb-4">Welcome to My App</h1>
-      <motion.div className="">
-
-      </motion.div>
     </div>
   );
 }
