3f4e8de7d76bedb8c28b6e82bb24a70acbe34ae1
Hastingsamo.com <hezekiahhastings14@gmail.com>
2025-05-12 11:13:06 +0100

    feat: add animations to DetailLayout and improve mobile view layout

--- src/DetailLayout.jsx
+++ src/DetailLayout.jsx
@@ -13,68 +13,80 @@ const DetailLayout = ({
   return (
     <div className="flex flex-row min-h-screen">
       <div className="flex flex-col md:flex-row p-5 gap-8 bg-[rgb(249,249,249)] rounded-xl  flex-1">
-          <div className="hidden md:flex w-full gap-8">
-            <motion.div
-              animate={{ opacity: 1, y: 0, scale: 1 }}
-              transition={{ duration: 1 }}
-              initial={{ opacity: 0, scale: 0.8, y: 130 }}
-              whileInView={{ opacity: 1, scale: 1 }}
-              className="flex-1 p-8 bg-blue-300 rounded-xl shadow-md"
-            >
-              <h1 className="text-2xl font-bold mt-20">{title}</h1>
-              <p className="text-gray-600">{description}</p>
-
-              <div className="mt-4">{children}</div>
-            </motion.div>
-
-            {/* Right Section - Image Display */}
-            <motion.div
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
-              initial={{ opacity: 0, y: -50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              className="flex-1 flex justify-center items-start"
-            >
-              <img
-                src={image}
-                alt={title}
-                className="w-96 h-96 object-cover flex shadow-lg"
-              />
-            </motion.div>
-          </div>
-          <div className="flex-1 flex justify-center items-start ">
-            <img
-              src={image}
-              alt={title}
-              className="w-96 h-96 object-cover flex  shadow-lg"
-            />
-          </div>
-        </div>
-
-        {/* Mobile View */}
-        <div className="md:hidden flex flex-col items-center bg-[#c88d84]">
-          <div className="flex-1 flex justify-center items-center md:hidden">
-            <img
-              src={image}
-              alt={title}
-              className="w-full max-w-sm rounded-xl  object-cover shadow-lg"
-            />
-          </div>
-          <div className="flex-1 flex flex-col gap-4th rounded-3xl w-full mt-20 bg-[#e4c2bd] px-10 pt-3 md:hidden">
+        <div className="hidden md:flex w-full gap-8">
+          <motion.div
+            animate={{ opacity: 1, y: 0, scale: 1 }}
+            transition={{ duration: 1 }}
+            initial={{ opacity: 0, scale: 0.8, y: 130 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            className="flex-1 p-8 bg-blue-300 rounded-xl shadow-md"
+          >
             <h1 className="text-2xl font-bold mt-20">{title}</h1>
             <p className="text-gray-600">{description}</p>
 
             <div className="mt-4">{children}</div>
-          </div>
+          </motion.div>
 
-          <div className="hidden md:flex-1 md:flex md:flex-col md:gap-4 ">
-            <h1 className="text-2xl font-bold">{title}</h1>
-            <p className="text-gray-600">{description}</p>
-
-            <div className="mt-4">{children}</div>
-          </div>
+          {/* Right Section - Image Display */}
+          <motion.div
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            initial={{ opacity: 0, y: -50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            className="flex-1 flex justify-center items-start"
+          >
+            <img
+              src={image}
+              alt={title}
+              className="w-96 h-96 object-cover flex shadow-lg"
+            />
+          </motion.div>
+        </div>
+        <div className="flex-1 flex justify-center items-start ">
+          <img
+            src={image}
+            alt={title}
+            className="w-96 h-96 object-cover flex  shadow-lg"
+          />
         </div>
       </div>
+
+      {/* Mobile View */}
+      <div className="md:hidden flex flex-col items-center bg-[#c88d84]">
+        <motion.div
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+          initial={{ opacity: 0, y: -50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          className="flex-1 flex justify-center items-center md:hidden"
+        >
+          <img
+            src={image}
+            alt={title}
+            className="w-full max-w-sm mt-[2rem] rounded-xl  object-cover shadow-lg"
+          />
+        </motion.div>
+        <motion.div
+          animate={{ opacity: 1, y: 0, scale: 1 }}
+          transition={{ duration: 1 }}
+          initial={{ opacity: 0, scale: 0.8, y: 130 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          className="flex-1 flex flex-col gap-4th rounded-3xl w-full mt-20 bg-[#e4c2bd] px-10 pt-3 md:hidden"
+        >
+          <h1 className="text-2xl font-bold mt-20">{title}</h1>
+          <p className="text-gray-600">{description}</p>
+
+          <div className="mt-4">{children}</div>
+        </motion.div>
+
+        <div className="hidden md:flex-1 md:flex md:flex-col md:gap-4 ">
+          <h1 className="text-2xl font-bold">{title}</h1>
+          <p className="text-gray-600">{description}</p>
+
+          <div className="mt-4">{children}</div>
+        </div>
+      </div>
+    </div>
   );
 };
 
