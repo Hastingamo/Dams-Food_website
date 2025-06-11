3f0d0f58ac95cff3b75bb517df72407d3a8e0278
Hastingsamo.com <hezekiahhastings14@gmail.com>
2025-06-11 17:11:05 +0100

    ttt

--- src/LayOut.jsx
+++ src/LayOut.jsx
@@ -4,7 +4,7 @@ import Header from './Component/Header'
 import Footer from './Component/Footer' 
 function LayOut() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col overflow-hidden">
       <Header />
       <main className="flex-grow overflow-y-auto bg-gradient-to-b from-white to-gray-100">
         <Outlet />
