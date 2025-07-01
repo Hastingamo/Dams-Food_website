6e18e8c51104463b339be437561ff927cfd576b5
Hastingsamo.com <hezekiahhastings14@gmail.com>
2025-07-01 15:29:26 +0100

    feat: Remove Bootstrap dependency and clean up unused components

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,7 @@ function App() {
             <Route path="/Home" element={<Home />} />
             <Route path="/food" element={<Foods />} />
             <Route path="/cocktails" element={<CockTailss />} />
-            {/* <Route path="/cocktails/:id" element={<CockDetails />} />
-            <Route path="/food/:id" element={<FoodDetials />} />
-            <Route path="/foodDetails" element={<FoodDetials />} /> */}
+        
             <Route path="/SignUp" element={<SignUp />} />
             <Route path="/Login" element={<Login />} />
 
@@ -39,9 +37,7 @@ function App() {
             <Route path="/foodDetails" element={<FoodDetials />} />
             <Route path="AddToChart" element={<AddTochart/>} />
             <Route path="/CheckOut" element={<CheckOut/>} />
-            {/* Add more private routes as needed */}
           </Route>
-          {/* Add more routes as needed */}
         </Routes>
       </Router>
     </>
